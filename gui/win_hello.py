20fd4c468e417be11823fecdd56f896666a0afe5
yj1408 <dudwnstnlfl@naver.com>
2021-10-01 13:17:27 +0900

    gui 파일

---
+++ gui/win_hello.py
@@ -0,0 +1,18 @@
+# gui 프로그램 만들기 - tkinker 모듈 불러오기
+from tkinter import *
+
+def click():
+    print("안녕~ 파이썬!")
+
+root = Tk() # 루트 생성
+root.title("윈도우 창")
+root.geometry("250x100+300+300") #창의 크기(너비 x 높이 + x좌표 + y좌표)
+
+frame = Frame(root) #root 를 매개로 frame 객체 생성
+frame.pack()       #화면에 라벨, 버튼 출력(가운데 정렬)
+
+Label(frame, text="이름 : ").grid(row=0, column=0) #라벨 1행 1열
+Button(frame, text="확인", command=click).grid(row=1, columnspan=2)  # 버튼 명령 실행 command - click()의 괄호 생략
+
+
+root.mainloop()
